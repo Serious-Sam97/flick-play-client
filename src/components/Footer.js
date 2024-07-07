ca29df5b44b1342ec9e3d3e31c1062d825093581
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2024-07-07 12:07:48 -0300

    Added Material and ts

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer style={footerStyle}>
+            <p>© 2024 Flick Play</p>
+        </footer>
+    )
+}
+
+const footerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px',
+    position: 'fixed',
+    left: '0',
+    bottom: '0',
+    width: '100%'
+  };
+
+export default Footer;
