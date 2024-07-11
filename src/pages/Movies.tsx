b9fa6c0270c19ab6db510b720cf9bedae4206560
Serious-Sam97 <samzuckerberg.jar@gmail.com>
2024-07-11 18:32:07 -0300

    Added pagination on movies page

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import axios from 'axios';
 import Movie from '../components/Movie';
-import { Container, Grid } from '@mui/material';
+import { Box, Container, Grid, Pagination } from '@mui/material';
 
 interface MovieData {
     id: number;
@@ -11,12 +11,15 @@ interface MovieData {
 
 function Movies() {
     const [movies, setMovies] = useState<MovieData[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
     
     useEffect(() => {
         const fetchMovies = async () => {
             try {
-                const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&page=1`);
+                const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&page=${currentPage}`);
                 setMovies(response.data.results);
+                setTotalPages(response.data.total_pages);
                 console.log(movies);
             }
             catch(error) {
@@ -25,7 +28,7 @@ function Movies() {
         }
         
         fetchMovies();
-    }, []);
+    }, [currentPage]);
 
     if (!movies) {
         return(
@@ -33,18 +36,32 @@ function Movies() {
         );
     }
 
+    const changePage = (event: ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
+    }
+    
+
     return (
         <Container>
             <h1>Popular Movies</h1>
-            <Grid container spacing={12} style={moviesStyle}>
-                {movies.map(movie => {
-                    return (
-                        <Grid item xs={3}>
-                            <Movie key={`movie-${movie.id}`} name={movie.original_title} posterPath={movie.poster_path}/>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+            <div style={moviesStyle}>
+                <Grid container spacing={12}>
+                    {movies.map(movie => {
+                        return (
+                            <Grid item xs={3}>
+                                <Movie key={`movie-${movie.id}`} name={movie.original_title} posterPath={movie.poster_path}/>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+                <Box display={'flex'} justifyContent={'center'} alignItems={'center'} marginTop={'5%'}>
+                    <Pagination
+                        page={currentPage}
+                        count={totalPages}
+                        onChange={changePage}
+                    />
+                </Box>
+            </div>
         </Container>
     );
 }
